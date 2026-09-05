f21488aa195a0f9cfd15d761d1d2d14ae767b6b7
Jonathan González <inagonzalez73@gmail.com>
2026-09-04 20:36:28 -0500

    Configurar Django para Render

--- carthy_project/settings.py
+++ carthy_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = ['192.168.200.225', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
