2773f922572d5e9c14a6596289a6e611ebe605d1
Jonathan Gonzalez <inagonzalez73@gmail.com>
2026-09-15 15:10:19 -0500

    Add automatic admin creation

---
+++ create_admin.py
@@ -0,0 +1,26 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Carthy_project.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superusuario creado correctamente.")
+    else:
+        print("El superusuario ya existe.")
+else:
+    print("No se configuraron las variables del superusuario.")
